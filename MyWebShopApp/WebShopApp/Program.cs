3e48f231d602666dca794acf6999dcd1204cb131
dxn7i <d.ivanov2020@pmg-pernik.com>
2026-01-08 12:31:29 +0200

    part2

--- MyWebShopApp/WebShopApp/Program.cs
+++ MyWebShopApp/WebShopApp/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 using WebShopApp.Infrastructure.Data;
+using WebShopApp.Infrastructure.Data.Domain;
 
 namespace WebShopApp
 {
@@ -17,7 +18,7 @@ namespace WebShopApp
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
             {
                 options.SignIn.RequireConfirmedAccount = false;
                 options.Password.RequireDigit = false;
