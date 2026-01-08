3cf906b66a7713a14f2a56611481a1d958c78028
dxn7i <d.ivanov2020@pmg-pernik.com>
2026-01-08 12:46:27 +0200

    part6

--- MyWebShopApp/WebShopApp/Program.cs
+++ MyWebShopApp/WebShopApp/Program.cs
@@ -38,6 +38,7 @@ namespace WebShopApp
             builder.Services.AddTransient<ICategoryService, CategoryService>();
             builder.Services.AddTransient<IBrandService, BrandService>();
             builder.Services.AddTransient<IProductService, ProductService>();
+            builder.Services.AddTransient<IOrderService, OrderService>();
 
             var app = builder.Build();
 
